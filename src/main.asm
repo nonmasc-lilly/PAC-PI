0a808a17fd2bce59e53cca953fe449f9e8f09250
nonmasc-lilly <lilly.tau@outlook.com>
2025-05-08 18:09:06 +1000

    Increase code readability with macros

--- src/main.asm
+++ src/main.asm
@@ -1,17 +1,70 @@
 format elf64 executable
 entry _start
 
+SYS_READ  = 0
+SYS_WRITE = 0x01
+SYS_OPEN  = 0x02
+SYS_CLOSE = 0x03
+SYS_EXIT  = 0x3C
+
+macro syscall sys_code, [arg] {
+common
+    match any, sys_code \{ mov eax, sys_code \}
+    match =0, sys_code \{ xor eax, eax \}
+forward
+    match register =:==     =0,  arg \{ xor register, register \}
+    match register =:==  value,  arg \{ mov register,    value \}
+    match register =:== [value], arg \{ mov register,  [value] \}
+    match register =&== [value], arg \{ lea register,  [value] \}
+common
+    syscall
+}
+
+macro ifx expr*, identifier* {
+    local matched
+    matched equ FALSE
+    match a == b, expr \{
+        cmp a, b
+        jne identifier
+        matched equ TRUE
+    \}
+    match a =~== b, expr \{
+        cmp a, b
+        je identifier
+        matched equ TRUE
+    \}
+    match =FALSE, matched \{
+        err
+    \}
+}
+macro endifx identifier* {
+    label identifier
+}
+
+macro forever identifier* {
+    label identifier
+}
+macro endforever identifier* {
+    jmp identifier
+}
+
+macro strdef name*, [arg] {
+common
+    label name
+    dd @f-$-4
+forward
+    db arg
+common
+    @@:
+}
+
 segment readable executable
 _start:
     ; argc == 2 or err
     cmp qword [rsp], 0x02
     jne .err_argc
     ; Open file
-    mov eax, 0x02
-    mov rdi, [rsp+0x10]
-    mov esi, 0x00
-    mov edx, 777o
-    syscall
+    syscall SYS_OPEN, rdi := [rsp+0x10], esi := 0, edx := 777o
     mov qword [fd], rax
     
     ; Setup stack
@@ -20,11 +73,7 @@ _start:
     sub rsp, 0x100
    
     ; Read file
-    mov eax, 0x00
-    mov rdi, qword [fd]
-    mov rsi, rsp
-    mov edx, 0x100
-    syscall
+    syscall SYS_READ, rdi := qword [fd], rsi := rsp, edx := 0x100
 
     ; Setup rbx and registers
     xor ebx, ebx
@@ -35,199 +84,143 @@ _start:
     mov byte [rsp+0xF9], 0xFF
 
     ; main loop
-    .loop:
-        cmp byte [rsp+rbx], -1
-        jne .notip
-            cmp byte [rsp+rbx+1], -1
-            jne .notipip
+    forever .loop
+        ifx byte[rsp+rbx] = -1, .notip
+            ifx byte[rsp+rbx+1] = -1, .notipip
                 add bl, bl
                 add bl, 0x02
                 jmp .loop
-            .notipip:
-            cmp byte [rsp+rbx+1], -2
-            jne .notipin
+            endifx .notipip
+            ifx byte[rsp+rbx+1] = -2, .notipin
                 dec rsp
-                xor eax, eax
-                xor edx, edx
-                mov rsi, rsp
-                mov edx, 0x01
-                syscall
+                syscall SYS_READ, edi := 0, rsi := rsp, edx := 0x01
                 add bl, byte [rsp]
-                add bl, 0x02
                 inc rsp
-                jmp .loop
-            .notipin:
-            cmp byte [rsp+rbx+1], -3
-            jne .notipot
                 add bl, 0x02
                 jmp .loop
-            .notipot:
+            endifx .notipin
+            ifx byte[rsp+rbx+1] = -3, .notipot
+                add bl, 0x02
+                jmp .loop
+            endifx .notipot
             mov dl, [rsp+rbx+1]
             mov dl, [rsp+rdx]
             add bl, dl
             add bl, 0x02
             jmp .loop
-        .notip:
-        cmp byte [rsp+rbx], -2
-        jne .notin
+        endifx .notip
+        ifx byte[rsp+rbx] = -2, .notin
             add bl, 0x02
             jmp .loop
-        .notin:
-        cmp byte [rsp+rbx], -3
-        jne .notot
-            cmp byte [rsp+rbx+1], -1
-            jne .nototip
+        endifx .notin
+        ifx byte[rsp+rbx] = -3, .notot
+            ifx byte[rsp+rbx+1] = -1, .nototip
                 dec rsp
                 mov [rsp], bl
-                mov eax, 0x01
-                mov edi, eax
-                mov rsi, rsp
-                mov edx, edi
-                syscall
+                syscall SYS_WRITE, edi := eax, rsi := rsp, edx := edi
                 inc rsp
                 add bl, 0x02
                 jmp .loop
-            .nototip:
-            cmp byte [rsp+rbx+1], -2
-            jne .nototin
+            endifx .nototip
+            ifx byte[rsp+rbx+1] = -2, .nototin
                 dec rsp
-                xor eax, eax
-                xor edx, edx
-                mov rsi, rsp
-                mov edx, 0x01
-                syscall
-                mov eax, 0x01
-                mov edi, eax
-                mov rsi, rsp
-                mov edx, edi
-                syscall
+                syscall  SYS_READ, edi :=   0, rsi := rsp, edx := 0x01
+                syscall SYS_WRITE, edi := eax, rsi := rsp, edx := edi
                 inc rsp
                 add bl, 0x02
                 jmp .loop
-            .nototin:
-            cmp byte [rsp+rbx+1], -3
-            jne .nototot
+            endifx .nototin
+            ifx byte[rsp+rbx+1] = -3, .nototot
                 add bl, 0x02
                 jmp .loop
-            .nototot:
+            endifx .nototot
             mov dl, [rsp+rbx+1]
             mov dl, [rsp+rdx]
             dec rsp
             mov [rsp], dl
-            mov eax, 0x01
-            mov edi, eax
-            mov rsi, rsp
-            mov edx, edi
-            syscall
+            syscall SYS_WRITE, edi := eax, rsi := rsp, edx := edi
             inc rsp
             add bl, 0x02
             jmp .loop
-        .notot:
-        cmp byte [rsp+rbx], -4
-        jne .notrs
-            cmp byte [rsp+rbx+1], -1
-            jne .notrsip
+        endifx .notot
+        ifx byte[rsp+rbx] = -4, .notrs
+            ifx byte[rsp+rbx+1] = -1, .notrsip
                 add [rsp+0xFC], bl
                 add bl, 0x02
                 jmp .loop
-            .notrsip:
-            cmp byte [rsp+rbx+1], -2
-            jne .notrsin
+            endifx .notrsip
+            ifx byte[rsp+rbx+1] = -2, .notrsin
                 dec rsp
-                xor eax, eax
-                xor edx, edx
-                mov rsi, rsp
-                mov edx, 0x01
-                syscall
+                syscall SYS_READ, edi := 0, rsi := rsp, edx := 0x01
                 mov dl, byte [rsp]
                 add [rsp+0xFC], dl
                 inc rsp
                 add bl, 0x02
                 jmp .loop
-            .notrsin:
-            cmp byte [rsp+rbx+1], -3
-            jne .notrsot
+            endifx .notrsin
+            ifx byte[rsp+rbx+1] = -3, .notrsot
                 add bl, 0x02
                 jmp .loop
-            .notrsot:
+            endifx .notrsot
             mov dl, [rsp+rbx+1]
             mov dl, [rsp+rdx]
             add [rsp+0xFC], dl
             add bl, 0x02
             jmp .loop
-        .notrs:
-        cmp byte [rsp+rbx], -5
-        jne .notr0
+        endifx .notrs
+        ifx byte[rsp+rbx] = -5, .notr0
             add bl, 0x02
             jmp .loop
-        .notr0:
-        cmp byte [rsp+rbx], -6
-        jne .notr1
+        endifx .notr0
+        ifx byte[rsp+rbx] = -6, .notr1
             add bl, 0x02
             jmp .loop
-        .notr1:
-        cmp byte [rsp+rbx], -7
-        jne .notrn
+        endifx .notr1
+        ifx byte[rsp+rbx] = -7, .notrn
             add bl, 0x02
             jmp .loop
-        .notrn:
-        cmp byte [rsp+rbx], -8
-        jne .notex
-            mov eax, 0x3C
-            xor edi, edi
-            syscall
-        .notex:
-        cmp byte [rsp+rbx+1], -1
-        jne .notnoip
+        endifx .notrn
+        ifx byte[rsp+rbx] = -8, .notex
+            syscall SYS_EXIT, edi := 0
+        endifx .notex
+
+        ifx byte[rsp+rbx+1] = -1, .notnoip
             mov dl, [rsp+rbx]
             add [rsp+rdx], bl
             add bl, 0x02
             jmp .loop
-        .notnoip:
-        cmp byte [rsp+rbx+1], -2
-        jne .notnoin
+        endifx .notnoip
+        ifx byte[rsp+rbx+1] = -2, .notnoin
             mov dl, byte [rsp+rbx]
             mov r12, rdx
             dec rsp
-            mov eax, 0x00
-            mov edi, 0x00
-            mov rsi, rsp
-            mov edx, 0x01
-            syscall
+            syscall SYS_READ, edi := 0, rsi := rsp, edx := 0x01
             mov dl, [rsp]
             inc rsp
             add [rsp+r12], dl
             add bl, 0x02
             jmp .loop
-        .notnoin:
-        cmp byte [rsp+rbx+1], -3
-        jne .notnoot
+        endifx .notnoin
+        ifx byte[rsp+rbx+1] = -3, .notnoot
             add bl, 0x02
             jmp .loop
-        .notnoot:
-        cmp byte [rsp+rbx+1], -8
-        jne .notnoet
-            mov eax, 0x3C
-            xor edi, edi
-            syscall
-        .notnoet:
+        endifx .notnoot
+        ifx byte[rsp+rbx+1] = -8, .notnoet
+            syscall SYS_EXIT, edi := 0
+        endifx .notnoet
         mov dl, [rsp+rbx+1]
         mov dl, [rsp+rdx]
         xor rax, rax
         mov al, [rsp+rbx]
         add [rsp+rax], dl
         add bl, 0x02
-        jmp .loop
+    endforever .loop
 
-    mov eax, 0x3C
-    xor edi, edi
-    syscall
+    syscall SYS_EXIT, edi := 0
+
 .err_argc:
-    mov eax, 0x01
-    mov edi, eax
-    lea rsi, [ERR_argc_1+4]
-    mov edx, [ERR_argc_1]
-    syscall
+    ; display error message equiv to: printf("Usage: %s <input binary>", argv[0]) and exit
+    syscall SYS_WRITE, edi := eax, rsi &= [ERR_argc_1+4], edx := [ERR_argc_1]
     mov rsi, [rsp+0x08]
     xor edx, edx
     .err_argc.strlen:
@@ -237,31 +230,14 @@ _start:
         inc rsi
         jmp .err_argc.strlen
     .err_argc.strlen.end:
-    mov eax, 0x01
-    mov edi, eax
-    mov rsi, [rsp+0x08]
-    syscall
-    mov eax, 0x01
-    mov edi, eax
-    lea rsi, [ERR_argc_2+4]
-    mov edx, [ERR_argc_2]
-    syscall
-    mov eax, 0x3C
-    mov edi, 0x01
-    syscall
-    
-
+    syscall SYS_WRITE, edi := eax, rsi := [rsp+0x08]
+    syscall SYS_WRITE, edi := eax, rsi &= [ERR_argc_2+4], edx := [ERR_argc_1]
+    syscall SYS_EXIT, edi := 0x01
 
 segment readable
 
-ERR_argc_1:
-    dd @f-$-4
-    db "Usage: "
-@@:
-ERR_argc_2:
-    dd @f-$-4
-    db " <input binary>", 0x0A
-@@:
+strdef ERR_argc_1, "Usage: "
+strdef ERR_argc_2, " <input binary>", 0x0A
 
 segment readable writeable
 
